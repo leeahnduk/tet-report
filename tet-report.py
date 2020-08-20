98268cb7ac7dc9e5b778b6b23c3da416b060ff3a
leeahnduk <anhdle@cisco.com>
2020-08-20 16:12:06 +0800

    update

--- tet-report.py
+++ tet-report.py
@@ -17,6 +17,7 @@ from tqdm import tqdm as progress
 import urllib3
 import xlsxwriter
 from openpyxl import Workbook
+import re
 
 
 
@@ -183,6 +184,113 @@ def GetSwPackages(rc,uuid):
         print(resp.text)
     else:
         return resp.json()
+
+def GetApps(rc):
+    resp = rc.get('/applications')
+
+    if resp.status_code != 200:
+        print(URED + "Failed to retrieve Apps list" + CEND)
+        print(resp.status_code)
+        print(resp.text)
+    else:
+        return resp.json()
+
+def ShowApps(Apps):
+    AppsList = []
+    headers = ['Number', 'App Name', 'Author', 'App ID', 'Primary?']
+    for i,app in enumerate(Apps): AppsList.append([i+1,app["name"] , app['author'], app["id"], app['primary']])
+    table = columnar(AppsList, headers, no_borders=False)
+    print(table)
+
+def GetAppVersions(rc, appid):
+    resp = rc.get('/applications/' + appid + '/versions')
+
+    if resp.status_code != 200:
+        print(URED + "Failed to retrieve Apps list" + CEND)
+        print(resp.status_code)
+        print(resp.text)
+    else:
+        return resp.json()
+
+def GetLatestVersion(app_versions):
+    version =[]
+    try:
+        for vers in app_versions: 
+            if 'v' in vers["version"]: version.append(vers['version'])
+        return version[0]
+    except:
+        print(URED + "Failed to retrieve latest app version"+ CEND)
+
+def downloadConvs(rc,appIDs):
+    # Download Apps Conversation JSON files from Apps workspace
+    apps = []
+    limit = int(input ("How many conversation you want to download? "))
+    for appID in appIDs:
+        print('Downloading app details for '+appID + "into json file")
+        versions = GetAppVersions(rc,appID)
+        version = int(re.search(r'\d+', GetLatestVersion(versions)).group(0))
+        req_payload = {"version": version,
+               "limit": limit
+               }
+        resp = rc.post('/openapi/v1/conversations/%s'%appID, json_body=json.dumps(req_payload))
+        if resp.status_code == 200:
+            parsed_resp = json.loads(resp.content)
+            apps.append(parsed_resp)
+    
+    with open('all-conversations.json', "w") as config_file:
+                json.dump(apps, config_file, indent=4)
+                print("all-conversations.json created")
+
+
+
+def ShowConversationTet(convs):
+    """
+        Show All conversation and export to Excel file
+        Source IP | Source Filter Name | Destination IP | Destination Filter Name | Protocol | Port | Bytes | Packets
+        """
+    data_list = []
+    headers = ['Source IP', 'Destination IP', 'Protocol', 'Port', 'Bytes', 'Packets']
+    listconv = convs[0]
+    for x in listconv['results']:
+        data_list.append([x['src_ip'], x['dst_ip'], x['protocol'], x['port'], x['byte_count'], x['packet_count']]) 
+    table = columnar(data_list, headers, no_borders=False)
+    print(table)
+    with open('conversation.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',')
+        writer.writerow(i for i in headers)
+        for row in data_list:
+            writer.writerow(row)
+    
+    export_xlsfile = 'Apps_Conversation.xlsx'
+    workbook = xlsxwriter.Workbook(export_xlsfile)
+    bold = workbook.add_format({'bold': True})
+    worksheet = workbook.add_worksheet(name='Apps Conversation')
+    header_format = workbook.add_format()
+    header_format.set_bg_color('cyan')
+    header_format.set_bold()
+    header_format.set_font_size(13)
+    header_format.set_font_color('black')
+    worksheet.set_row(0, None)
+    worksheet.write_row(0,0,headers,header_format)
+    i=1
+    firstline = True
+    with open('conversation.csv', 'r') as f:
+        for row in csv.reader(f):
+            if firstline:    #skip first line
+                firstline = False
+                continue
+            worksheet.write_row(i,0,row)
+            i += 1
+    worksheet.set_column(0, 0, 20)
+    worksheet.set_column(1, 1, 20)
+    i =2
+    while i < 6:
+        worksheet.set_column(i, i, 15)
+        i += 1
+    workbook.close()
+    print ('Writing csv file to %s with %d columns' % (export_xlsfile, len(headers)))
+    os.remove('conversation.csv')
+
 
 def ShowSwPackages(packages):
     columns = None
@@ -847,17 +955,191 @@ def get_flow_topSrcService(rc):
         workbook.close()
         print ('Writing csv file to %s with %d columns' % (export_csvfile, len(csv_header)))
 
+def filterToString(invfilter):
+    if 'filters' in invfilter.keys():
+        query=[]
+        for x in invfilter['filters']:
+            if 'filters' in x.keys():
+                query.append(filterToString(x))
+            elif 'filter' in x.keys():
+                query.append(x['type'] + filterToString(x['filter']))
+            else:
+                query.append(x['field'].replace('user_','*')+ ' '+ x['type'] + ' '+ str(x['value']))
+        operator = ' '+invfilter['type']+' '
+        return '('+operator.join(query)+')'
+    else:
+        return invfilter['field']+ ' '+ invfilter['type'] + ' '+ str(invfilter['value'])
 
+def selectTetApps(apps):
+    # Return App IDa for one or many Tetration Apps that we choose
+    print (Cyan + "\nHere are all Application workspaces in your cluster: " + CEND)
+    ShowApps(apps)
+    choice = input('\nSelect which Tetration Apps (Number, Number) above you want to download polices: ')
+
+    choice = choice.split(',')
+    appIDs = []
+    for app in choice:
+        if '-' in app:
+            for app in range(int(app.split('-')[0])-1,int(app.split('-')[1])):
+                appIDs.append(resp.json()[int(app)-1]['id'])
+        else:
+            appIDs.append(apps[int(app)-1]['id'])
+    return appIDs
+
+def downloadPolicies(rc,appIDs):
+    # Download Policies JSON files from Apps workspace
+    apps = []
+    for appID in appIDs:
+        print('Downloading app details for '+appID + "into json file")
+        apps.append(rc.get('/openapi/v1/applications/%s/details'%appID).json())
+        #json_object = json.load(apps)
+    for app in apps:
+        with open('./'+app['name'].replace('/','-')+'.json', "w") as config_file:
+            json.dump(apps, config_file, indent=4)
+            print(app['name'].replace('/','-')+".json created")
+    return apps
+
+def convApps2xls(rc):
+    AllApps = GetApps(rc)
+    scopes = GetApplicationScopes(rc)
+    apps = []
+    appIDs = selectTetApps(AllApps)
+    apps.append(downloadPolicies(rc, appIDs))
+    print (BLINK + CRED +'Processing Application data ........ '+ CEND)
+    #print (json.dumps(apps, indent=4))
+
+    # Load in the IANA Protocols
+    protocols = {}
+    try: 
+        with open('protocol-numbers-1.csv') as protocol_file:
+            reader = csv.DictReader(protocol_file)
+            for row in reader:
+                protocols[row['Decimal']]=row
+    except IOError:
+        print('%% Could not load protocols file')
+        return
+    except ValueError:
+        print('Could not load improperly formatted protocols file')
+        return
+    
+    for app in apps[0]:
+        workbook = xlsxwriter.Workbook(app['name'].replace('/','-')+'.xlsx')
+        bold = workbook.add_format({'bold': True})
+
+        if 'clusters' in app.keys():
+            worksheet = workbook.add_worksheet(name='App Servers')
+            cell_format = workbook.add_format()
+            cell_format.set_bg_color('cyan')
+            cell_format.set_bold()
+            cell_format.set_font_color('black')
+            worksheet.set_row(0, None)
+            worksheet.write_row(0,0,['Hostname','IP','Cluster Membership'],cell_format)
+            i=1
+            clusters = app['clusters']
+            for cluster in clusters:
+                hosts = []
+                for node in cluster['nodes']:
+                    hosts.append(node['name'])
+                    worksheet.write_row(i,0,[node['name'],node['ip'],cluster['name']])
+                    i+=1
+            worksheet.set_column(0, 0, 30)
+            worksheet.set_column(1, 1, 15)
+            worksheet.set_column(2, 2, 30)
+
+        if 'inventory_filters' in app.keys():
+            i=1
+            worksheet = workbook.add_worksheet(name='External Groups')
+            cell_format = workbook.add_format()
+            cell_format.set_text_wrap()
+            header_format = workbook.add_format()
+            header_format.set_bg_color('cyan')
+            header_format.set_bold()
+            header_format.set_font_color('black')
+            worksheet.set_row(0, None)
+            worksheet.write_row(0,0,['Inventory Filter Name', 'IP Addresses', 'Filter Definition'],header_format)
+            worksheet.set_column(0, 0, 30)
+            worksheet.set_column(1, 1, 60, cell_format)
+            worksheet.set_column(2, 2, 50, cell_format)
+
+            filters = app['inventory_filters']
+            for invfilter in filters:
+                #print (json.dumps(invfilter, indent=4))
+                ipSet = resolveFilter(rc, invfilter)
+                #print (ipSet)
+                worksheet.write_row(i,0,[invfilter['name'], str(ipSet), filterToString(invfilter['query'])])
+                i+=1
+
+        if 'default_policies' in app.keys():
+            i=1
+            worksheet = workbook.add_worksheet(name='Policies')
+            header_format = workbook.add_format()
+            header_format.set_bg_color('cyan')
+            header_format.set_bold()
+            header_format.set_font_color('black')
+            worksheet.set_row(0, None)
+            worksheet.write_row(0,0,['Consumer Group','Provider Group','Services'],header_format)
+            worksheet.set_column(0, 0, 30)
+            worksheet.set_column(1, 1, 30)
+            worksheet.set_column(2, 2, 30)
+
+            policies = app['default_policies']
+            for policy in policies:
+                pols = {}
+                for rule in policy['l4_params']:
+                    if 'port' in rule:
+                        if rule['port'][0] == rule['port'][1]:
+                            port = str(rule['port'][0])
+                        else:
+                            port = str(rule['port'][0]) + '-' + str(rule['port'][1])
+                    else:
+                        port = None
+
+                    if port == None:
+                        try:
+                            pols[protocols[str(rule['proto'])]['Keyword']] = []
+                        except:
+                            pols['PROTO-'+str(rule['proto'])]=[]
+                    elif protocols[str(rule['proto'])]['Keyword'] in pols.keys():
+                        pols[protocols[str(rule['proto'])]['Keyword']].append(port)
+                    else:
+                        pols[protocols[str(rule['proto'])]['Keyword']] = [port]
+
+                policy_list = []
+                for key, val in pols.items():
+                    #print(key,val)
+                    if len(val)>0:
+                        policy_list.append('{}={}'.format(key,', '.join(val)))
+                    else:
+                        policy_list.append(key)
+                        
+                worksheet.write_row(i,0,[policy["consumer_filter_name"],policy["provider_filter_name"],'; '.join(policy_list)])
+                i+=1
+        
+        workbook.close()
+        print (app['name'].replace('/','-')+'.xlsx created for policies conversion to CSV')
+
+
+def resolveFilter(rc, filters):# return all IP and hosts for a specific filters
+    ipSet = []
+    body = json.dumps({'filter':filters['query']})
+   
+    resp = rc.post('/inventory/search',json_body=body)
+    if resp:
+        ips = resp.json()
+        for i in ips['results']:
+            ipSet.append(i['ip'])
+
+    return ipSet
 
 def main():
     print (BLINK + BOLD+ CGREEN + "Welcome to Tetration Report !!!" + CEND)
-    print (BOLD+ Cyan+ UNDERLINE + ITALIC +"Build report for Tetration, Command: report and sub command: workloads or flows. Use ? or help for more information"+ CEND)
+    print (BOLD+ Cyan+ UNDERLINE + ITALIC +"Build report for Tetration, Command: report and sub command: workloads or flows or apps. Use ? or help for more information"+ CEND)
     rc = CreateRestClient()
     command = input ("tetcli #  "+ CEND)
 
     # report 
     if command == "report h" or command =="report help" or command =="report ?": 
-        print (BOLD+ CYELLOW + "Build report for Tetration, sub command: workloads or flows  "+ CEND)
+        print (BOLD+ CYELLOW + "Build report for Tetration, sub command: workloads or flows or apps  "+ CEND)
     if command == "report workloads" or command == "report wl" or command == "report workloads ?" or command == "report workloads h" or command == "report workloads help" or command == "report wl ?" or command == "report wl h" or command == "report wl help":
         print (BOLD+ CYELLOW + "Build report for Tetration workloads, sub command: all or detail or stats or software or vulnerabilities or processes "+ CEND)
         print (BOLD+ CYELLOW + "All - Report all installed workloads in your cluster in all scopes  "+ CEND)
@@ -869,6 +1151,10 @@ def main():
     if command == "report flows" or command == "report flow" or command == "report flows ?" or command == "report flows h" or command == "report flows help" or command == "report flow ?" or command == "report flow h" or command == "report flow help":
         print (BOLD+ CYELLOW + "inv - Detail flow communication report about a subnet in a VRF from time (t0) to time(t1) "+ CEND)
         print (BOLD+ CYELLOW + "top - Top Talkers/Destination/Service report in excel for a scope from time (t0) to time(t1). Sub command: talkers, servers, sports, dports "+ CEND)
+    if command == "report apps" or command == "report app" or command == "report apps ?" or command == "report apps h" or command == "report apps help" or command == "report app ?" or command == "report app h" or command == "report app help":
+        print (BOLD+ CYELLOW + "Build report for Tetration Apps, sub command: policies or conversation "+ CEND)
+        print (BOLD+ CYELLOW + "Policies - Report policies in xlsx format for a specific Application  "+ CEND)
+        print (BOLD+ CYELLOW + "Conversation - Report conversation in xlsx format for a specific Application  "+ CEND)
     if command == "report workloads all" or command == "report wl all" or command == "report workloads a" or command == "report wl a": 
         sensors = GetSensors(rc)
         print (BOLD+ CYELLOW + "\nHere are all Software Sensors in your cluster: " + CEND)
@@ -941,6 +1227,14 @@ def main():
         get_flow_topSrcService(rc)
     if command == "report flow top dports" or command == "report flows top dports" or command == "report flow top dp" or command == "report flows top dp":
         get_flow_topDestService(rc)
+    if command == "report apps policies" or command == "report app policies" or command == "report apps pol" or command == "report app pol":
+        convApps2xls(rc)
+    if command == "report apps conversation" or command == "report app conversation" or command == "report apps conv" or command == "report app conv":
+        AllApps = GetApps(rc)
+        appIDs = selectTetApps(AllApps)
+        downloadConvs(rc, appIDs)
+        with open('all-conversations.json') as config_file:
+            ShowConversationTet(json.load(config_file))
 
 
 if __name__ == "__main__":
